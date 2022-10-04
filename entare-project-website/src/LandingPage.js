927fa04d562cd79d460852091abf023dd523e837
Your Name <mollenwambui@gmail.com>
2022-10-04 16:23:37 +0300

    landingpage

---
+++ entare-project-website/src/LandingPage.js
@@ -0,0 +1,30 @@
+import React ,{useState}from "react";
+import "./LandingPage.css"
+
+const LandingPage=()=>{
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li><a href="#"></a> Home</li>
+                    <li><a href="#"></a> About Entare</li>
+                    <li><a href="#"></a> The Team</li>
+                    <li><a href="#"></a> Contact Us</li>
+                </ul>
+            </nav>
+
+
+
+
+
+
+
+
+
+
+        
+        </div>
+    )
+}
+
+export default LandingPage
